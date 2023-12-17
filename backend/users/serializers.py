11aeeeb46aeb8758deaccdff16902987dbf59278
Motlatsi Moea <122087932+motlatsimoea@users.noreply.github.com>
2023-12-17 04:52:43 +0200

    added jwt auth and login

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import CustomUser, Profile
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
@@ -39,5 +40,50 @@ class CustomUserProfileSerializer(serializers.ModelSerializer):
 
         return instance
 
+class UserSerializerWithToken(serializers.ModelSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+    
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'group', 'isAdmin', 'token']
+    
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)   
+    
+    def get_isAdmin(self, obj):
+        return obj.is_staff 
+    
+
+class UserUpdateSerializerWithToken(serializers.ModelSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+    profile = ProfileSerializer()
+    
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'group' 'password', 'profile']
+    
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token) 
+        
+    def update(self, instance, validated_data):
+        profile_data = validated_data.pop('profile')
+        profile      = instance.profile
+        
+        instance.username = validated_data.get('username', instance.username)
+        instance.email    = validated_data.get('email', instance.email)   
+        instance.password = validated_data.get('password', instance.password)
+        instance.save()
+        
+        profile.first_name = profile_data.get('first_name', profile.first_name)
+        profile.last_name = profile_data.get('last_name', profile.last_name)
+        profile.user_info = profile_data.get('user_info', profile.user_info)
+        profile.image = profile_data.get( 'image', profile.image)
+        profile.country = profile_data.get( 'country', profile.country)
+        profile.save()
+        
+        return instance
 
 
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,18 +1,44 @@
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
+from django.contrib.auth.hashers import make_password
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from .serializers import CustomUserProfileSerializer, ProfileSerializer
+from .serializers import CustomUserProfileSerializer, ProfileSerializer, UserSerializerWithToken, UserUpdateSerializerWithToken
 from .models import *
 
-@api_view(['POST'])
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+        
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        serializer = UserSerializerWithToken(self.user).data
+        
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+    
+
+@api_view(['POST']) 
 def register(request):
-    if request.method == 'POST':
-        serializer = CustomUserProfileSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    data = request.data
+    
+    try:
+        user = CustomUser.objects.create(
+            username = data['username'],
+            email = data['email'],
+            group = data['group'],
+            password = make_password(data['password'])
+        )
+        
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'user with this email already exists!'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+    
 
 @api_view(['PUT'])
 @permission_classes([IsAuthenticated])
@@ -39,7 +65,7 @@ def update_profile(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) """
 
 
-@api_view(['GET'])
+""" @api_view(['GET'])
 #@permission_classes([IsAuthenticated])
 def get_user_profile(request, username):
     if request.method == 'GET':
@@ -49,4 +75,42 @@ def get_user_profile(request, username):
             profile_serializer = ProfileSerializer(profile, many=False)
             return Response(profile_serializer.data, status=status.HTTP_200_OK)
         except (CustomUser.DoesNotExist, Profile.DoesNotExist):
+            return Response({'error': 'User profile not found.'}, status=status.HTTP_404_NOT_FOUND) """
+            
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user_profile(request, username):
+    if request.method == 'GET':
+        try:
+            user = CustomUser.objects.get(username=username)
+            user_profile = Profile.objects.get(user=user)
+            
+            
+            #Serialize the Data
+            user_profile_serializer = CustomUserProfileSerializer(user_profile, many=False)
+            
+            
+            combined_data = {
+                'user_profile': user_profile_serializer.data,
+                
+            }
+            return Response(combined_data, status=status.HTTP_200_OK)
+        except CustomUser.DoesNotExist:
+            return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+        except Profile.DoesNotExist:
             return Response({'error': 'User profile not found.'}, status=status.HTTP_404_NOT_FOUND)
+        
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])   
+def update_user_profile(request):
+    if request.method == 'PUT':
+        user = request.user
+        serializer = UserUpdateSerializerWithToken(user, data=request.data, many=False)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
